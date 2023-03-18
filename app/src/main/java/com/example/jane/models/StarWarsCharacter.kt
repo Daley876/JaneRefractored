7544eedea79c955728b26d0c2fb9890beb8dc7c0
sheckardodaley <sheckardodaley@gmail.com>
2023-03-17 23:18:06 -0400

    removed redundant properties from model class

--- app/src/main/java/com/example/jane/models/StarWarsCharacter.kt
+++ app/src/main/java/com/example/jane/models/StarWarsCharacter.kt
@@ -6,17 +6,6 @@ data class StarWarsCharacter(
     @SerializedName("id")
     val characterId : Int,
      var name: String,
-     val height: Double,
-     val gender: String,
-     val mass : Int,
      var species: String?,
      val image: String?,
-    @SerializedName("hair_color")
-     var hairColor: String?,
-    @SerializedName("skin_color")
-     var skinColor: String?,
-    @SerializedName("eye_color")
-     var eyeColor: String?,
-    @SerializedName("homeworld")
-     var homeWorld: String?
 )

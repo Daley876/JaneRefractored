231898126280541450f288207fc02631a0c8ede4
sheckardodaley <sheckardodaley@gmail.com>
2023-03-20 20:57:37 -0400

    updated gradle and removed old class

--- app/src/main/java/com/example/jane/models/HomeWorld.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jane.models
-
-sealed class HomeWorld {
-    data class SingleHomeWorld(val homeworld: String) : HomeWorld()
-    data class ListHomeWorld(val homeworlds: List<String>) : HomeWorld()
-}
